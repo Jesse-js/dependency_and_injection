610da6575fee16c4c6a6c07463a1b3de1ecbf954
Jesse-js <jesse-jjs@hotmail.com>
2024-06-05 09:22:33 -0300

    feat: adds multiple dependencies

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 require_once __DIR__ . '/Cliente.php';
 require_once __DIR__ . '/CepServiceReal.php';
@@ -9,6 +9,9 @@ require_once __DIR__ . '/Produto.php';
 require_once __DIR__ . '/PDFGenerator.php';
 require_once __DIR__ . '/XLSGenerator.php';
 
+require_once __DIR__ . '/Venda.php';
+require_once __DIR__ . '/Mailer.php';
+
 echo (new Cliente(new CepServiceFake))->cadastrar();
 echo '<br>';
 echo (new Cliente(new CepServiceReal))->cadastrar();
@@ -16,3 +19,23 @@ echo '<br>';
 echo (new Produto(new PDFGenerator))->gerarRelatorio();
 echo '<br>';
 echo (new Produto(new XLSGenerator))->gerarRelatorio();
+
+echo '<br>';
+echo '<hr>';
+echo '<br>';
+
+
+$pdf = new PDFGenerator();
+
+$mailer = new Mailer([
+    'host' => 'smtp.gmail.com',
+    'port' => 587,
+    'auth' => true,
+    'username' => 'username',
+    'password' => 'password',
+    'secure' => 'tls',
+    'debug' => true,
+    'email' => '4gU4M@example.com',
+]);
+
+(new Venda($pdf, $mailer))->notificar();
